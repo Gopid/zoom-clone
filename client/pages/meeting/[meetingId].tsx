1bde3d899b1cbb6e121ef19a3b51760977667e45
Gopid <gyure.akos@gmail.com>
2021-05-24 16:28:09 +0200

    Disable service worker in development mode

--- client/pages/meeting/[meetingId].tsx
+++ client/pages/meeting/[meetingId].tsx
@@ -1,10 +1,43 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useRef, useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+function WebCam({ requestedMedia }: any) {
+  const videoRef: any = useRef(null);
+  const [mediaStream, setMediaStream] = useState<any>(null);
+
+  function handleCanPlay() {
+    videoRef.current.play();
+  }
+
+  useEffect(() => {
+    async function enableStream() {
+      const stream: any = await navigator.mediaDevices.getUserMedia(requestedMedia);
+      setMediaStream(stream);
+
+      videoRef.current.srcObject = stream;
+    }
+
+    if (!mediaStream) {
+      enableStream();
+    } else {
+      return function cleanup() {
+        mediaStream.getTracks().forEach((track: any) => {
+          track.stop();
+        });
+      };
+    }
+  }, [mediaStream, requestedMedia]);
+
+  return <video ref={videoRef} onCanPlay={handleCanPlay} autoPlay playsInline muted />;
+}
+
+const CAPTURE_OPTIONS = {
+  audio: false,
+  video: { facingMode: 'environment' },
+};
+
 const Meeting: FunctionComponent = () => {
-  const router = useRouter();
-  const { meetingId } = router.query;
-  return <h1>{meetingId}</h1>;
+  return <WebCam requestedMedia={CAPTURE_OPTIONS} />;
 };
 
 export default Meeting;
